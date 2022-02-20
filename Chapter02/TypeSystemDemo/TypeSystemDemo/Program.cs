252d397a094cfd6ef9de0e68b91af8003374bcff
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-02-20 19:56:10 +0200

    integer numeric types

--- Chapter02/TypeSystemDemo/TypeSystemDemo/Program.cs
+++ Chapter02/TypeSystemDemo/TypeSystemDemo/Program.cs
@@ -49,31 +49,31 @@ namespace TypeSystemDemo
             byte someByte = 10;
             Console.WriteLine($"typeof(someByte)={someByte.GetType()}; minValue(byte)={byte.MinValue}; maxValue(byte)={byte.MaxValue}");
 
-            // sbyte - 8 bits
+            // sbyte <=> System.SByte - 8 bits
             sbyte someSignedByte = -10;
             Console.WriteLine($"typeof(someSignedByte)={someSignedByte.GetType()}; minValue(sbyte)={sbyte.MinValue}; maxValue(sbyte)={sbyte.MaxValue}");
 
-            // ushort - 16 bits
+            // ushort <=> System.UInt16 - 16 bits
             ushort someUnsignedShort = 19;
             Console.WriteLine($"typeof(someUnsignedShort)={someUnsignedShort.GetType()}; minValue(ushort)={ushort.MinValue}; maxValue(ushort)={ushort.MaxValue}");
 
-            // short - 16 bits
+            // short <=> System.Int16 - 16 bits
             short someShort = -30;
             Console.WriteLine($"typeof(someShort)={someShort.GetType()}; minValue(short)={short.MinValue}; maxValue(short)={short.MaxValue}");
 
-            // uint - 32 bits
+            // uint <=> System.UInt32 - 32 bits
             uint someUnsignedInt = 1000;
             Console.WriteLine($"typeof(someUnsignedInt)={someUnsignedInt.GetType()}; minValue(uint)={uint.MinValue}; maxValue(uint)={uint.MaxValue}");
 
-            // int - 32 bits
+            // int <=> System.Int32 - 32 bits
             int someInteger = -1000;
             Console.WriteLine($"typeof(someInteger)={someInteger.GetType()}; minValue(int)={int.MinValue}; maxValue(int)={int.MaxValue}");
 
-            // ulong - 64 bits
+            // ulong <=> System.UInt64 - 64 bits
             ulong someUnsignedLong = 1000000;
             Console.WriteLine($"typeof(someUnsignedLong)={someUnsignedLong.GetType()}; minValue(ulong)={ulong.MinValue}; maxValue(ulong)={ulong.MaxValue}");
 
-            // long - 64
+            // long <=> System.Int64 - 64
             long someLong = -1000000;
             Console.WriteLine($"typeof(someLong)={someLong.GetType()}; minValue(long)={long.MinValue}; maxValue(long)={long.MaxValue}");
         }
