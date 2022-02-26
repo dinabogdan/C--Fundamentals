e0c7d905ba778d7d7c2bf7a4c3871586b1e3ef64
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-02-26 20:29:40 +0200

    functions & methods

---
+++ Chapter03/MethodsAndFunctions/MethodsAndFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace MethodsAndFunctions
+{
+    class Program
+    {
+        int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int GiveMeFive()
+        {
+            return 5;
+        }
+
+        static void Main(string[] args)
+        {
+            var five = GiveMeFive();
+
+            Console.WriteLine(five);
+        }
+    }
+}
