32b186fea8fafbbd01d3d2dd5373b7b5f51aadbb
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-04-02 11:37:36 +0300

    methods and functions with parameters

--- Chapter03/MethodsAndFunctions/MethodsAndFunctions/Program.cs
+++ Chapter03/MethodsAndFunctions/MethodsAndFunctions/Program.cs
@@ -1,13 +1,48 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace MethodsAndFunctions
 {
     class Program
     {
-        int Sum(int x, int y)
+        // Methods with parameters
+
+        static int Sum(int x, int y)
         {
             return x + y;
+        }
+
+        // out parameters method definition
+        static int Divide(int x, int y, out int remainder)
+        {
+            remainder = x % y;
+            return x / y;
+        }
+
+        // default parameters
+        static int DivideByNumberOrDefault2(int x, int y = 2)
+        {
+            return x / y;
+        }
+
+        // named parameters
+        static void PrintCustomerDetails(string name, string phoneNumber, string address, string firstName,
+            string middleName, int age)
+        {
+            // writing to the console.
+        }
+
+
+        // params - parameter
+
+        static int SumNumbers(params int[] numbers)
+        {
+            var sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
+
+            return sum;
         }
 
         static int GiveMeFive()
@@ -18,8 +53,26 @@ namespace MethodsAndFunctions
         static void Main(string[] args)
         {
             var five = GiveMeFive();
-
             Console.WriteLine(five);
+
+            var sum = Sum(5, 6);
+            Console.WriteLine($"Sum is {sum}");
+
+            // out parameters
+            bool tryParse = int.TryParse("s", out var parsedResult);
+
+            Console.WriteLine($"Parsing result is: {tryParse} for parsedResult: {parsedResult}");
+
+            var divisionResult = Divide(10, 3, out var remainder);
+            Console.WriteLine($"DivisionResult: {divisionResult} with remainder: {remainder}");
+
+            var result = DivideByNumberOrDefault2(10, 5);
+            Console.WriteLine(result);
+
+            PrintCustomerDetails(phoneNumber: "0744444400", name: "John", address: "some address", firstName: "Doe",
+                middleName: "No middle name", age: 10);
+
+            Console.WriteLine($"Sum is {SumNumbers(1, 2, 3, 4, 5, 6, 7, 8)}");
         }
     }
 }
