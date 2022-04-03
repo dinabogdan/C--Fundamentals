8b60d75baf339fdd756980ce9561ad3ab7596187
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-04-03 22:43:12 +0300

    inheritance & virtual methods

---
+++ Chapter05/AdvancedOOP/Animals.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedOOP
+{
+    public class Animal
+    {
+        public string Name { get; init; }
+        public string Food { get; init; }
+        public string Habitat { get; init; }
+
+        private int _hunger = 10;
+
+        public virtual void MakeNoise()
+        {
+            Console.WriteLine($"The animal {Name} is making noise!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"The animal {Name} is eating {Food}");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine($"The animal {Name} is sleeping in {Habitat}");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine($"The dog {Name} is making noise!");
+        }
+
+        public bool HasCollar { get; init; }
+    }
+}
--- Chapter05/AdvancedOOP/Program.cs
+++ Chapter05/AdvancedOOP/Program.cs
@@ -2,11 +2,30 @@
 
 namespace AdvancedOOP
 {
+    public class Base
+    {
+
+    }
+
+    public class Derived : Base
+    {
+
+    }
+
+    public class MoreDerived : Derived
+    {
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animal cat = new Animal { Name = "Tom", Food = "CatFood", Habitat = "House" };
+            Animal dog = new Dog { Name = "Berkley", Food = "DogFood", Habitat = "House", HasCollar = true };
+
+            cat.MakeNoise();
+            dog.MakeNoise();
         }
     }
 }
