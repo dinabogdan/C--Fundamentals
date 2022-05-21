abf8ff9eb3f45ddad0ec8de95c29e81c8d087549
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-05-21 15:14:10 +0300

    basic operations in collections

---
+++ Chapter07/Collections/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Generic collections vs Raw collections
+
+            // ArrayList - a raw collection
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("2");
+            arrayList.Add(new { FirstName = "John" });
+
+            // Hashtable - a raw collection
+            Hashtable hashTable = new Hashtable();
+
+            // var first = (string) arrayList[0]; - it will produce an InvalidCastException
+
+            // List<T> - a generic collection
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+
+            // Dictionary<TKey, TValue> - a generic
+            Dictionary<string, int> stringToInts = new Dictionary<string, int>();
+            stringToInts.Add("1", 1);
+
+            // How to instantiate & initialize collections?
+            List<int> conciseInts = new() { 1, 2, 3, 4, 5 };
+            Dictionary<string, int> conciseStringToInts = new Dictionary<string, int>
+            {
+                ["1"] = 1,
+                ["2"] = 2,
+                ["3"] = 3
+            };
+
+            // 2. Immutable and ReadOnly collections
+
+            // readonly collections
+            ReadOnlyCollection<int> readonlyCollectionInts = new ReadOnlyCollection<int>(ints);
+            ints.Add(3);
+            Console.WriteLine($"Element on position 2: {readonlyCollectionInts[2]}");
+
+            // immutable collections
+            ImmutableArray<int> immutableArray = ImmutableArray.Create(new[] { 1, 2, 3, 4, 5 });
+            var secondInstance = immutableArray.Add(6);
+            Console.WriteLine($"Element on position 5: {secondInstance[5]}");
+
+            // 3. Add elements to collections
+            stringToInts.Add("2", 2);
+
+
+            // 4. Updating collection elements
+            ints[0] = 10;
+            stringToInts["3"] = 3;
+
+            Console.WriteLine($"Ints position 0 {ints[0]}");
+            Console.WriteLine($"stringToInts key '3' {stringToInts["3"]}");
+
+            // 5. Removing elements from collection
+            stringToInts.Remove("3");
+            Console.WriteLine($"stringToInts key '3' {stringToInts["3"]}");
+        }
+    }
+}
