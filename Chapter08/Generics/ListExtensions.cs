411f96eed01df38135e6a8e0d999938bc185bbff
EUROPE\bogdandina <bogdandina@microsoft.com>
2022-06-01 13:44:16 +0300

    generic methods

---
+++ Chapter08/Generics/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public static class ListExtensions
+    {
+        public static List<T> AddOneElement<T>(this List<T> elements, T element)
+        {
+            elements.Add(element);
+            return elements;
+        }
+    }
+}
--- Chapter08/Generics/Program.cs
+++ Chapter08/Generics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Generics
 {
@@ -24,6 +26,9 @@ namespace Generics
 
     internal class Program
     {
+
+        static Cage<T> ANewCage<T>(T animal, double size) => new Cage<T> { Animal = animal, Size = size };
+
         static void Main(string[] args)
         {
             var dog = new Dog
@@ -51,6 +56,13 @@ namespace Generics
                 Animal = cat,
                 Size = 0.5
             };
+
+            // 1. Generic method (static or as an instance method)
+            var secondCage = ANewCage(dog, 1.0);
+
+            // 2. Generic extension method
+            var listOfInts = new List<int> { };
+            listOfInts.AddOneElement(1);
         }
     }
 }
